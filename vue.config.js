e9934d385becda9eb72b4479020b1b18043760b4
ssbtn <ssbtn>
2019-05-18 23:01:51 +0900

    build方式の変更

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-    configureWebpack: {
-        resolve: {
-            alias: {
-            'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' webpack 1 用
-        }
-      }
-    },
       baseUrl: './'
 }
