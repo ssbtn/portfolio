752dc130459e2132cb0881b6684f881b66838f65
ssbtn <ssbtn>
2019-05-19 17:36:27 +0900

    色々詳細化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,26 @@
 <template>
   <div>
-    <v-carousel
-    :cycle="false"
-    >
-      <v-carousel-item
-        v-for="(item,i) in items"
-        :key="i"
-        :src="item.src"
+    <v-card>
+      <v-img
+        class="white--text"
+        height="400px"
+        :src="page_image"
       >
         <v-flex>
-            <h3 class="display-3">Welcome to the site</h3>
-            <span class="subheading">Lorem ipsum dolor sit amet, pri veniam forensibus id. Vis maluisset molestiae id, ad semper lobortis cum. At impetus detraxit incorrupte usu, repudiare assueverit ex eum, ne nam essent vocent admodum.</span>
+            <h3 class="display-3">{{page_title}}</h3>
+            <span class="subheading">{{page_description}}</span>
         </v-flex>
-      </v-carousel-item>
-    </v-carousel>
+      </v-img>
+    </v-card>
     <v-tabs fixed-tabs>
       <v-tab
-        v-for="n in 3"
-        :key="n"
-      >
-        Item {{ n }}
+        v-for="section in sections"
+        :key="section.id"
+        v-on:click="$vuetify.goTo(section.to, scrollOpt)">
+        {{ section.title }}
       </v-tab>
     </v-tabs>
-    <v-layout text-xs-center mt-5 px-3 py-3 id="about">
+    <v-layout text-xs-center mt-5 px-3 py-3 id="aboutme">
       <v-flex xs12>
         <span class="display-2 font-weight-bold">About Me</span>
       </v-flex>
@@ -31,53 +29,142 @@
       <v-flex xs12>
         <v-avatar size="250" color="grey lighten-4">
           <img
-            src="https://vuetifyjs.com/apple-touch-icon-180x180.png"
+            :src="my_icon"
             alt="avatar"
           >
         </v-avatar>
-        <p class="headline font-weight mt-2">Gwen Danielle Merida</p>
+        <p class="headline font-weight mt-2">{{my_name}}</p>
         <p
           class="title font-weight-thin mt-3"
-        >Hello I'm a Full Stack Web Developer and iOS Developer from Philippines. Have 5 years work experience in IT Industry.</p>
+        >{{my_profile}}</p>
       </v-flex>
     </v-layout>
-    <v-timeline>
+    <v-divider></v-divider>
+    <v-layout text-xs-center mt-5 px-3 py-3 id="portfolio">
+      <v-flex xs12>
+        <span class="display-2 font-weight-bold">Portfolio</span>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12 sm6 offset-sm3
+        v-for="portfolio in portfolios"
+        :key="portfolio.id"
+      >
+        <v-card>
+          <v-img
+            :src="portfolio.image"
+            aspect-ratio="2.75"
+          ></v-img>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">{{portfolio.title}}</h3>
+              <div> {{ portfolio.text }} </div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-container>
+      <v-layout>
+        <v-flex></v-flex>
+      </v-layout>
+    </v-container>  
+    <v-divider></v-divider>
+    <v-layout text-xs-center mt-5 px-3 py-3 id="history">
+      <v-flex xs12>
+        <span class="display-2 font-weight-bold">History</span>
+      </v-flex>
+    </v-layout>
+    <div class="my-5">
+    <v-timeline class="mt-4">
       <v-timeline-item
-        v-for="n in 3"
-        :key="n"
+        v-for="turning_point in turning_points"
+        :key="turning_point.id"
         color="red lighten-2"
         large
       >
         <template v-slot:opposite>
-          <span>Tus eu perfecto</span>
+          <span>{{turning_point.year}}</span>
         </template>
         <v-card class="elevation-2">
-          <v-card-title class="headline">Lorem ipsum</v-card-title>
+          <v-card-title class="headline">{{turning_point.title}}</v-card-title>
           <v-card-text>
-            Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
+            {{turning_point.text}}
           </v-card-text>
         </v-card>
       </v-timeline-item>
     </v-timeline>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    data () {
-      return {
-        items: [
+    data: () => ({
+        page_image: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg',
+        page_title: 'Welcome to the ssbtn`s site',
+        page_description: `page_desctiption`,
+
+        sections: [
           {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+            id: '1',
+            title: 'About Me',
+            to: '#aboutme'
+          },
+                    {
+            id: '2',
+            title: 'Portfolio',
+            to: '#portfolio'
           },
           {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+            id: '3',
+            title: 'History',
+            to: '#history'
           }
-        ]
-      }
-    }
+        ],
+
+        my_icon: 'https://vuetifyjs.com/apple-touch-icon-180x180.png',
+        my_name: 'ssbtn',
+        my_profile: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n aaaaaaaaaaaa\naaaaaaaaaaaaaaaa',
+
+        portfolios: [
+          {
+            id: 1,
+            image: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
+            title: 'Kangaroo Valley Safari',
+            text: 'Lorem ipsum dolor sit amet.'
+          }
+        ],
+
+        turning_points: [
+          {
+            id: 1,
+            year: '2018',
+            title: 'Kangaroo Valley Safari',
+            text: 'Lorem ipsum dolor sit amet.'
+          },
+          {
+            id: 2,
+            year: '2019',
+            title: 'Kangaroo Valley Safari',
+            text: 'Lorem ipsum dolor sit amet.'
+          },
+          {
+            id: 3,
+            year: '2020',
+            title: 'Kangaroo Valley Safari',
+            text: 'Lorem ipsum dolor sit amet.'
+          }
+        ],
+
+        src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+        card_text: 'Lorem ipsum dolor sit amet.',
+
+        scrollOpt: {
+        duration: 1500,
+        offset: 10,
+        easing: "easeInOutCubic"
+        }
+    })
   }
 </script>
